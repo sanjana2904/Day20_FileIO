670061d8ba60cad2b0a4a492b3cad823943a8c6b
sanjana k <sanjanarthi@gmail.com>
2021-04-10 11:27:12 +0530

    Add project files.

---
+++ Day20_FileIO/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+
+namespace Day20_FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileExists();
+            ReadAllLines();
+            ReadAllText();
+            Console.ReadKey();
+        }
+
+        public static void FileExists()
+        {
+            //String path = "C:\\Users\\inyadav\\source\\repos\\Day20_FileIO\\Day20_FileIO\\Example.txt";
+            String path = "Example.txt";
+            if (File.Exists(path))
+                Console.WriteLine("File exists");
+            else
+                Console.WriteLine("File doesn't exists");
+        }
+
+        public static void ReadAllLines()
+        {
+            //String path = "C:\\Users\\inyadav\\source\\repos\\Day20_FileIO\\Day20_FileIO\\Example.txt";
+
+            String path = "C:\\Users\\sanja\\source\\repos\\Day20_FileIO\\Day20_FileIO\\Example.txt";
+
+            //String path = "Example.txt";
+            String[] lines;
+            //C: \Users/inyadav\source\repos\Day20_FileIO\Day20_FileIO
+            lines = File.ReadAllLines(path);
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+
+        }
+
+        public static void ReadAllText()
+        {
+            //String path = "C:\\Users\\inyadav\\source\\repos\\Day20_FileIO\\Day20_FileIO\\Example.txt";
+            //String path = "Example.txt";
+            String path = "C:\\Users\\sanja\\source\\repos\\Day20_FileIO\\Day20_FileIO\\Example.txt";
+            String lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine(lines);
+
+        }
+
+        public static void FileCopy()
+        {
+            //String path = "C:\\Users\\inyadav\\source\\repos\\Day20_FileIO\\Day20_FileIO\\Example.txt";
+            //String path = "Example.txt";
+            String path = "C:\\Users\\sanja\\source\\repos\\Day20_FileIO\\Day20_FileIO\\Example.txt";
+            String copypath = "C:\\Users\\inyadav\\source\\repos\\Day20_FileIO\\Day20_FileIO\\ExampleNew.txt";
+
+            File.Copy(path, copypath);
+
+        }
+
+        public static void DeleteFile()
+        {
+            //String path = "C:\\Users\\inyadav\\source\\repos\\Day20_FileIO\\Day20_FileIO\\Example.txt";
+            //String path = "Example.txt";
+            String path = "C:\\Users\\sanja\\source\\repos\\Day20_FileIO\\Day20_FileIO\\Example.txt";
+
+            File.Delete(path);
+
+        }
+
+        public static void ReadFromStreamReader()
+        {
+            //String path = "C:\\Users\\inyadav\\source\\repos\\Day20_FileIO\\Day20_FileIO\\Example.txt";
+            String path = "Example.txt";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                String s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+        }
+
+        public static void WriteUsingStreamWriter()
+        {
+            //String path = "C:\\Users\\inyadav\\source\\repos\\Day20_FileIO\\Day20_FileIO\\Example.txt";
+            String path = "Example.txt";
+
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("Hellow World - .Net is awesome");
+                sr.Close();
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+
+    }
+
+}
+   
